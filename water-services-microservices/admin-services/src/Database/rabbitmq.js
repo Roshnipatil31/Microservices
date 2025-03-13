1c24c0290099ff828038915e7ceaf803c0e97984
Roshnipatil31 <Roshni@digi9.co.in>
2025-03-13 13:00:43 +0530

    added admin

---
+++ water-services-microservices/admin-services/src/Database/rabbitmq.js
@@ -0,0 +1,21 @@
+ // Load environment variables
+ const amqp = require('amqplib');
+ const dotenv = require('dotenv');
+ dotenv.config();
+ 
+//  const RABBITMQ_URL = process.env.RABBITMQ_URL; // Load from .env
+
+const RABBITMQ_URL = process.env.RABBITMQ_URL || 'amqps://ptlzfjzs:i4WN7QK4IklWCapW1pR02lstp_kElC40@duck.lmq.cloudamqp.com/ptlzfjzs';
+ 
+ async function connectRabbitMQ() {
+     try {
+         const connection = await amqp.connect(RABBITMQ_URL);
+         const channel = await connection.createChannel();
+         console.log('✅ Connected to CloudAMQP');
+         return channel;
+     } catch (error) {
+         console.error('❌ RabbitMQ connection error:', error);
+     }
+ }
+ 
+ module.exports = connectRabbitMQ;
